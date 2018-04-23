6b6932ede6d056462760dcfd2c4517550c8846f2
Katie Day <katie@springload.co.nz>
2018-04-24 10:22:40 +1200

    fix issue #10

--- src/quicktube.js
+++ src/quicktube.js
@@ -83,7 +83,7 @@ class Quicktube {
         const playerOptions = this.isVimeo ? {
             autopause: 0,
         } : {
-            showInfo: 0,
+            showinfo: 0,
             autohide: 1,
             color: 'white',
             playerapi: 'ytplayer',
